d70a25839dcc42c2b8758d65b6c6ebfe5e4ac2e8
devhan-hub <hanabduman@gmail.com>
2025-05-15 12:12:05 +0300

    Add reusable CustomHeader and CustomFooter components

---
+++ frontend/js/layout.js
@@ -0,0 +1,25 @@
+class CustomHeader extends HTMLElement {
+  connectedCallback() {
+      fetch('components/header.html')
+          .then(response => response.text())
+          .then(data => {
+              this.innerHTML = data;
+          })
+          .catch(error => console.error('Error loading header:', error));
+  }
+}
+
+class CustomFooter extends HTMLElement {
+  connectedCallback() {
+      fetch('components/footer.html')
+          .then(response => response.text())
+          .then(data => {
+              this.innerHTML = data;
+          })
+          .catch(error => console.error('Error loading footer:', error));
+  }
+}
+
+customElements.define('custom-header', CustomHeader);
+customElements.define('custom-footer', CustomFooter);
+  
